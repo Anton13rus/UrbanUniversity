0a40478091d90a82ebc34f22dece3a444b810bfe
Anton13rus <antonsarankin@gmail.com>
2024-12-01 11:45:40 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+number_of_comleted = 12
+number_of_hours_spent = 1.5
+name_of_cours = 'Python'
+print('Название курса: ', name_of_cours,
+      'Количество выполненных заданий: ', number_of_comleted,
+       'Количество затраченных часов: ',number_of_hours_spent,
+     'Количество времени на одно задание: ',  (number_of_hours_spent / number_of_comleted))
