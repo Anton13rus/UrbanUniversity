84a176ed331e54fa218cc2a5ea1a5942c32da59c
Anton13rus <antonsarankin@gmail.com>
2024-12-02 17:32:17 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1, 2, 3, 4, True, 'Hello', 3.4
+print(immutable_var)
+
+#immutable_var[3] = 9
+# Кортеж относится к неизменяемым типам данных
+
+mutable_list = [1, 2, 3.2, 'head', 'soul', False]
+mutable_list[5] = True
+mutable_list[0] = 9
+mutable_list[3] = 'legs'
+print(mutable_list)
